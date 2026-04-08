b21146b291b87c7005d6694a0303bb77954d9a77
GAMEIUN PISONET <GAMEIUN PISONET@DESKTOP-T7E6NRS>
2026-04-09 07:42:13 +0800

    added connection

---
+++ src/alecerdb/connection.java
@@ -0,0 +1,24 @@
+
+package alecerdb;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class connection {
+    private static final String URL = "jdbc:mysql://localhost:3306/alecer_db?zeroDateTimeBehavior=CONVERT_TO_NULL";
+    private static final String USER = "root";
+    private static final String PASSWORD = "";
+    
+    public static Connection getConnection(){
+        Connection conn = null;
+        
+        try{
+            conn = DriverManager.getConnection( URL, USER, PASSWORD);
+            System.out.println("Database Connected Successfully!"); 
+        }catch (SQLException e){
+            System.out.println("Connection Failed.");
+            e.printStackTrace();
+        }
+        return conn;
+    }
+}

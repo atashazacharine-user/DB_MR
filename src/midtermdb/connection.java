215b1f92cbdab129ea331000e0967bf188bc890e
eronm <eronm@10.0.2.56>
2026-04-17 18:53:43 +0800

    Added CONNECTION

---
+++ src/midtermdb/connection.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package midtermdb;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class connection {
+    private static final String URL = "jdbc:mysql://localhost:3306/alecer_mr";
+    private static final String USER = "root";
+    private static final String PASSWORD = "";
+    
+    public static Connection getConnection(){
+        Connection conn = null;
+        
+        try{
+            conn = DriverManager.getConnection( URL, USER, PASSWORD);
+            System.out.println("Database Connected Successfully!"); 
+        }catch (SQLException e){
+            System.out.println("Connection Failed.");
+            e.printStackTrace();
+        }
+        return conn;
+    }
+}
